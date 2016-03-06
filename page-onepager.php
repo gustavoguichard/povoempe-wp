3f7056866091327c2dc56e520447aeb90673c1e6
Gustavo Guichard <gustavoguichard@gmail.com>
2016-03-05 22:08:19 -0300

    Add hardcoded facebook events and print their JSON

--- page-onepager.php
+++ page-onepager.php
@@ -229,9 +229,42 @@ get_header(); ?>
     </div>
   </div>
   <!-- Contact Ends! -->
+  <?php
+    $string = file_get_contents(get_template_directory().'/query.json');
+    $json_a = json_decode($string, true);
+    $valid_events = array_filter($json_a['events']['data'], function($event) {
+      $today = date(c);
+      $has_start = empty($event['start_time']);
+      $has_end = isset($event['end_time']);
+      $is_scheduled = ($has_end & $event['end_time'] >= $today) || ($has_start & $event['start_time'] >= $today);
+      return($is_scheduled);
+    });
+  ?>
   <div class="services">
     <a class="anchor" id="agenda"></a>
     <h2>Agenda</h2>
+    <?php if(empty($valid_events)) : ?>
+      <h3>Sem items na agenda</h3>
+    <?php else: ?>
+      <?php foreach($valid_events as $event) : ?>
+        <?php
+          $start_date = new DateTime($event['start_time']);
+          $end_date = new DateTime($event['end_time']);
+        ?>
+        <article class="agenda-item">
+          <a href="https://www.facebook.com/events/<?php echo $event['id'];?>/" class="agenda-link" target="blank">
+            <?php if(isset($event['cover']['source'])) : ?>
+              <img src="<?php echo $event['cover']['source'] ;?>" alt="Imagem do evento" class="event-image" />
+            <?php endif; ?>
+            <h3 class="event-name"><?php echo $event['name'];?></h3>
+            <p class="event-date">
+              <?php echo $start_date->format('d/m/Y');?>
+              <?php if($end_date > $start_date) { echo ' - '.$end_date->format('d/m/Y'); } ?>
+            </p>
+          </a>
+        </article>
+      <?php endforeach; ?>
+    <?php endif; ?>
   </div>
 
   <!-- Map -->
