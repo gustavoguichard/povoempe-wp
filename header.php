9c4b8ee2a73fb5b8f90bed17491e23ae2a26d882
Gustavo Guichard <gustavoguichard@gmail.com>
2016-03-06 09:45:42 -0300

    Refactor header menu function

--- header.php
+++ header.php
@@ -13,6 +13,28 @@
 </head>
 <body <?php body_class(); ?>>
 
+<?php
+  function top_side_menu($menu_name) {
+    $menu_list ='';
+    if ( ($locations = get_nav_menu_locations()) && isset($locations[$menu_name]) ) {
+      $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
+      $menu_items = wp_get_nav_menu_items($menu->term_id);
+      $menu_list = '<ul class="'.$menu_name.'">';
+
+      foreach ( (array) $menu_items as $menu_item ) {
+        $title = $menu_item->title;
+        $id = get_post_meta( $menu_item->ID, '_menu_item_object_id', true );
+        $menuurl = $menu_item->url;
+        $menu_list .= '<li class="item">
+                        <a href="'.$menuurl.'" class="title">'.$title.'</a>
+                      </li>';
+      }
+      $menu_list .= '</ul>';
+    }
+    return($menu_list);
+  }
+?>
+
 <div class="main">
 
   <!-- Header -->
@@ -23,167 +45,8 @@
     <!-- Menu -->
     <div class="menu">
       <div class="container">
-
-        <?php
-
-          $menu_name = 'left-side-menu';
-          $menu_list ='';
-
-          if ( ($locations = get_nav_menu_locations()) && isset($locations[$menu_name]) ) {
-
-            $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
-
-            $menu_items = wp_get_nav_menu_items($menu->term_id);
-            $menu_list = '<ul class="left-side-menu">';
-            $isfrontpage = is_front_page();
-            $submenu = false;
-            $count = 0;
-
-            // foreach
-            foreach ( (array) $menu_items as $menu_item ) {
-              $count++;
-              $title = $menu_item->title;
-              $id = get_post_meta( $menu_item->ID, '_menu_item_object_id', true );
-              $link = get_page_link( $id );
-              $menuurl = $menu_item->url;
-
-              if ( $menu_item->menu_item_parent == "0" ) {
-
-                if ( $submenu ) {
-                  $menu_list .= '</ul></li>';
-                  $submenu = false;
-
-                } else {
-                  if ( $count>1 ) { $menu_list .= '</li>'; }
-
-                }
-
-                if ( $isfrontpage) {
-                  $menu_list .= '<li><a href="' . $menuurl . '">' . $title . '</a>';
-
-                } else {
-                  if (strpos($menuurl, '#') !== FALSE) {
-                    $menu_list .= '<li><a href="' . $url .$menuurl. 'href">' . $title . '</a>';
-
-                  } else {
-                    $menu_list .= '<li><a href="' . $menuurl . '">' . $title . '</a>';
-                  }
-
-                }
-
-              } else {
-                if ( !$submenu ) {
-                  $submenu = true;
-                  $menu_list .=  '<ul class="sub-menu">';
-
-                }
-
-                $menu_list .= '<li class="item';
-
-                if (get_the_ID()==$id) {
-                  $menu_list .= ' current-menu-item';
-                }
-
-                $menu_list .= '">
-                <a href="'.$menuurl.'" class="title">'.$title.'</a>';
-                $menu_list .= '</li>';
-
-                if (count($menu_items)==$count) {
-                  $menu_list .= '</ul></li>';
-                  $submenu = false;
-                }
-              }
-            }
-            // foreach ends!
-
-            $menu_list .= '</ul>';
-
-          }
-
-          echo $menu_list;
-
-        ?>
-
-        <?php
-
-          $menu_name = 'right-side-menu';
-          $menu_list ='';
-
-          if ( ($locations = get_nav_menu_locations()) && isset($locations[$menu_name]) ) {
-
-            $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
-
-            $menu_items = wp_get_nav_menu_items($menu->term_id);
-            $menu_list = '<ul class="right-side-menu">';
-            $isfrontpage = is_front_page();
-            $submenu = false;
-            $count = 0;
-
-            // foreach
-            foreach ( (array) $menu_items as $menu_item ) {
-              $count++;
-              $title = $menu_item->title;
-              $id = get_post_meta( $menu_item->ID, '_menu_item_object_id', true );
-              $link = get_page_link( $id );
-              $menuurl = $menu_item->url;
-
-              if ( $menu_item->menu_item_parent == "0" ) {
-
-                if ( $submenu ) {
-                  $menu_list .= '</ul></li>';
-                  $submenu = false;
-
-                } else {
-                  if ( $count>1 ) { $menu_list .= '</li>'; }
-
-                }
-
-                if ( $isfrontpage) {
-                  $menu_list .= '<li><a href="' . $menuurl . '">' . $title . '</a>';
-
-                } else {
-                  if (strpos($menuurl, '#') !== FALSE) {
-                    $menu_list .= '<li><a href="' . $url .$menuurl. 'href">' . $title . '</a>';
-
-                  } else {
-                    $menu_list .= '<li><a href="' . $menuurl . '">' . $title . '</a>';
-                  }
-
-                }
-
-              } else {
-                if ( !$submenu ) {
-                  $submenu = true;
-                  $menu_list .=  '<ul class="sub-menu">';
-
-                }
-
-                $menu_list .= '<li class="item';
-
-                if (get_the_ID()==$id) {
-                  $menu_list .= ' current-menu-item';
-                }
-
-                $menu_list .= '">
-                <a href="'.$menuurl.'" class="title">'.$title.'</a>';
-                $menu_list .= '</li>';
-
-                if (count($menu_items)==$count) {
-                  $menu_list .= '</ul></li>';
-                  $submenu = false;
-                }
-              }
-            }
-            // foreach ends!
-
-            $menu_list .= '</ul>';
-
-          }
-
-          echo $menu_list;
-
-        ?>
-
+        <?php echo top_side_menu('left-side-menu'); ?>
+        <?php echo top_side_menu('right-side-menu'); ?>
       </div>
     </div>
      <!-- Menu Ends -->
