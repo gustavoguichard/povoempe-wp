c709ae01e163d15d8dfbc5a2efb4cb33f3a8f8c4
Gustavo Guichard <gustavoguichard@gmail.com>
2016-10-05 16:09:32 -0300

    Adds ability to save albums

--- facebook-login.php
+++ facebook-login.php
@@ -9,6 +9,15 @@
 get_header(); ?>
 <?php
 global $fb;
+
+function write_data_to_file( $data, $filename ) {
+  $fileUrl = get_template_directory().'/data/'.$filename.'.json';
+  $myFile = fopen($fileUrl, 'w') or die('Unable to open file: '.$fileUrl);
+  fwrite($myFile, $data) or die('Unable to write file: '.$fileUrl);
+  fclose($myFile) or die('Unable to close file: '.$fileUrl);
+}
+
+
 if(isset($fb) && current_user_can( 'publish_posts' ) && isset($_REQUEST['code'])) {
   try {
     $token = $fb->getRedirectLoginHelper()->getAccessToken();
@@ -21,14 +30,25 @@ if(isset($fb) && current_user_can( 'publish_posts' ) && isset($_REQUEST['code'])
   }
 
   if (isset($token)) {
-    $response = $fb->get('povoempepoa?fields=events{start_time,end_time,name,id,cover{id,source,offset_y},description}', $token);
-    $graphNode = $response->getGraphNode();
-    $fileUrl = get_template_directory().'/data/events.json';
-    $myfile = fopen($fileUrl, 'w') or die("Unable to open file!");
-    fwrite($myfile, $graphNode) or die("Unable to write file!");
-    fclose($myfile) or die("Unable to close file!");
-    echo '<meta http-equiv="refresh" content="0;url='.get_option("siteurl").'?agenda=updated">';
+    $pageUrl = 'povoempepoa';
+    $eventsResponse = $fb->get($pageUrl.'?fields=events{start_time,end_time,name,id,cover{id,source,offset_y},description}', $token);
+    write_data_to_file($eventsResponse->getGraphNode(), 'events');
+
+    $albumsResponse = $fb->get($pageUrl.'?fields=albums{photos{name,images},name,location,cover_photo}', $token);
+    write_data_to_file($albumsResponse->getGraphNode(), 'albums');
+    echo '<meta http-equiv="refresh" content="0;url='.get_option("siteurl").'?facebook_data=updated">';
   }
 }
 
+/*
+
+  location == "Espaço Povo Em Pé - Belem Novo"
+  name = Nome do album
+  photos->data = [{name, images = [{height, source, width}], id}]
+  source /p320x320/ para média
+  source /p480x480/ (ou 600 ou 720) para Aberta
+  source /p130x130/ para thumb
+  cover_photo = {created_time,name,id} ID deve ser o mesmo do array
+}
+*/
 ?>
